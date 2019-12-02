8678c82952253e7d7bc63fe7e3f55eaecd4bccbc
iemtsev <emtzev@gmail.com>
2019-12-03 01:43:36 +0300

    A minor refactoring

--- src/main/com/heisenbug/demo/utils/MetadataPublisher.kt
+++ src/main/com/heisenbug/demo/utils/MetadataPublisher.kt
@@ -9,7 +9,7 @@ class MetadataPublisher(className: String) {
     }
 
     fun publishVideo(testName: String) {
-        val filename = getLatestFile("video").name.replace("selenoid","")
+        val filename = getLatestFile("video").name
         print ("##teamcity[testMetadata testName='com.heisenbug.demo.$className.$testName' type='video' value='metadata/videos/$filename' name='$filename']")
     }
 }
